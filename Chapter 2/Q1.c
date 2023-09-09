ae119cc0bd30cafe5d7ddfe1ada3cce13a046f95
Shubhodeep Mondal <mondalshubhodeep30924@gmail.com>
2023-09-09 22:29:53 +0530

    added chapter 2 q1

---
+++ Chapter 2/Q1.c
@@ -0,0 +1,37 @@
+/*
+    Calculation of sum of digits of a 5-digit no. given by the user.
+    Date:- 09/09/2023
+*/
+
+# include<stdio.h>
+
+int main()
+{
+    int n,sum;
+
+    printf("Enter a 5-digit no: ");
+    scanf("%d", &n);
+
+      //calculating the one's place number
+    
+    sum = n%10;
+    //calculating the two's place number
+    
+    sum = sum + (n%100)/10;
+    
+    //calculating the third's place number
+    
+    sum = sum + (n%1000)/100;
+    
+    //calculating the forth's place number
+    
+    sum = sum + (n%10000)/1000;
+    
+    //calculating fifth's place number
+    
+    sum = sum + (n%100000)/10000;
+   
+    printf("The sum of digits of %d is %d" , n , sum);
+    
+    return 0;
+}
