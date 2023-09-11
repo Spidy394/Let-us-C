4068cc43967eb4dc60cb1a07eda5595eb8bdc2f5
Shubhodeep Mondal <mondalshubhodeep30924@gmail.com>
2023-09-11 20:18:18 +0530

    added windchill

---
+++ Chapter 2/6-WindChill.c
@@ -0,0 +1,24 @@
+/*
+    calculation of wind chill factor.
+    date: 11/09/2023
+*/
+
+# include <stdio.h>
+# include <math.h>
+
+int main()
+{
+    float t, v, wcf;
+
+    // taking inputs of t and v
+    printf("Enter termperature in fahrenheit: ");
+    scanf("%f", &t);
+    printf("Enter wind speed in mph: ");
+    scanf("%f", &v);
+
+    // calculating wind chill factor 
+    wcf = 35.74 + 0.6215*t + ( 0.4275*t - 35.75 ) * (pow(v,0.16));
+
+    printf("Wind Chill factor: %f fahrenheit", wcf);
+    return 0;
+}
