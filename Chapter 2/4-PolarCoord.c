80fcfadff38ad65e6556e6588519bcf55300d84c
Shubhodeep Mondal <mondalshubhodeep30924@gmail.com>
2023-09-11 17:45:39 +0530

    added 4-polarCoord.c

---
+++ Chapter 2/4-PolarCoord.c
@@ -0,0 +1,27 @@
+/*
+    convertion of cartesian to polar coordinate.
+    date: 11/09/2023.
+*/
+
+# include <stdio.h>
+# include <math.h>
+int main()
+{
+    float x, y, radialDistance, angle;
+
+    // taking inputs for x and y coordinates
+    printf("Enter x-coordinate: ");
+    scanf("%f", &x);
+    printf("Enter y-coordinate: ");
+    scanf("%f", &y);
+
+    // finding radialDistance
+    radialDistance = sqrt(pow(x, 2) + pow(y, 2));
+
+    // finding angle in radian
+    angle = atan2(y,x);
+
+    printf("The Polar Coordinarte is: (%.2f,%.2f radians)", radialDistance , angle);
+
+    return 0;
+}
