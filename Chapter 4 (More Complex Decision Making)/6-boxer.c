74381ab93d37f3cd1d51922da60dbecb8457ba4b
Shubhodeep Mondal <mondalshubhodeep30924@gmail.com>
2023-10-04 13:34:52 +0530

    added 7

--- Chapter 4 (More Complex Decision Making)/6-boxer.c
+++ Chapter 4 (More Complex Decision Making)/6-boxer.c
@@ -7,6 +7,25 @@
 
 int main()
 {
-    
+    float weight;
+
+    printf("Enter weight of boxer (in pounds)");
+    scanf("%f", &weight);
+
+    if (weight < 115)
+        printf("Flyweight");
+
+    else if ((weight>=115) && (weight<=121))
+        printf("Bantamweight");
+
+    else if ((weight>=122) && (weight<=153))
+        printf("Featherweight");
+
+    else if ((weight>=154) && (weight<=189))
+        printf("Middleweight");
+
+    else
+        printf("Heavyweight");
+
     return 0;
 }
