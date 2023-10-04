afe414e610859f462c285cbe89b68c69ee1a26be
Shubhodeep Mondal <79023855+Spidy394@users.noreply.github.com>
2023-10-04 22:43:25 +0530

    added comments in 1-grossSalary.c

--- Chapter 1 (Getting Started)/1-grossSalary.c
+++ Chapter 1 (Getting Started)/1-grossSalary.c
@@ -7,16 +7,19 @@
 
 int main()
 {
+    // declaration of variables
     int base;
     float da, hra, gross;
 
+    // taking input from user
     printf("Enter base salary: ");
     scanf("%d", &base);
 
-    da = base*0.4;
-    hra = base*0.2;
-    gross = base + da + hra;
+    da = base*0.4; // calculating dearness allowance
+    hra = base*0.2; // calculating house rent allowance
+    gross = base + da + hra; // calculating gross salary
 
+    // printing gross salary
     printf("Gross salary: %.2f", gross);
     
     return 0;
