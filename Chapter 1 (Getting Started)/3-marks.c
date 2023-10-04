24a06a6cbc4d61d7cecc21f1799ed056c2858209
Shubhodeep Mondal <79023855+Spidy394@users.noreply.github.com>
2023-10-04 22:53:27 +0530

    added comments to  3-marks.c

--- Chapter 1 (Getting Started)/3-marks.c
+++ Chapter 1 (Getting Started)/3-marks.c
@@ -7,9 +7,11 @@
 
 int main()
 {
+    // variable declaration 
     int s1, s2, s3, s4, s5, total;
     float agg, per;
 
+    // taking inputs
     printf("Enter marks of Subject 1 out of 100: ");
     scanf("%d", &s1);
     printf("Enter marks of Subject 2 out of 100: ");
@@ -20,11 +22,12 @@ int main()
     scanf("%d", &s4);
     printf("Enter marks of Subject 5 out of 100: ");
     scanf("%d", &s5);
+    
+    total = s1 + s2 + s3 + s4 + s5 ; // calculating total marks 
+    agg =  total / 5.0; // calculating aggregate marks
+    per = (total/500.0)*100; // calculating percentage
 
-    total = s1 + s2 + s3 + s4 + s5 ;
-    agg =  total / 5.0;
-    per = (total/500.0)*100;;
-
+    // printing aggregate marks and percentge
     printf("Aggregate marks: %.2f", agg);
     printf("\nPercentage: %.2f%%", per);
 
