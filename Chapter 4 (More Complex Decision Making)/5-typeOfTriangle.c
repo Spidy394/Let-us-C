997f229a854e26ae412c5c929161a604a3fdca5a
Shubhodeep Mondal <mondalshubhodeep30924@gmail.com>
2023-10-04 13:14:24 +0530

    added 6

--- Chapter 4 (More Complex Decision Making)/5-typeOfTriangle.c
+++ Chapter 4 (More Complex Decision Making)/5-typeOfTriangle.c
@@ -1,4 +1,44 @@
 /* 
     to determine the type of triangle.
-    
+    date:20/09/23
 */
+
+# include <stdio.h>
+# include <math.h>
+
+int main()
+{
+    float a, b, c;
+
+    printf("Enter side a: ");
+    scanf("%f", &a);
+    printf("Enter side b: ");
+    scanf("%f", &b);
+    printf("Enter side c: ");
+    scanf("%f", &c);
+
+     // Check if the input forms a valid triangle
+    if (a + b > c && a + c > b && b + c > a) 
+    {   
+        // Check for equilateral triangle
+        if (a == b && b == c)
+            printf("It is an Equilateral Triangle\n");
+
+        // Check for isosceles triangle
+        else if (a == b || b == c || a == c)
+            printf("It is an Isosceles Triangle\n");
+
+        // Check for right-angled triangle
+        else if (pow(c, 2) == (pow(a, 2) + pow(b, 2)) || pow(a, 2) == (pow(b, 2) + pow(c, 2)) || pow(b, 2) == (pow(a, 2) + pow(c, 2))) 
+            printf("It is a Right Angle Triangle\n");
+
+        // If none of the above conditions are met, it's a scalene triangle
+        else 
+            printf("It is a Scalene Triangle\n");
+    }
+
+    else
+        printf("It is not a triangle\n");
+            
+    return 0;
+}
---
+++ Chapter 4 (More Complex Decision Making)/6-boxer.c
@@ -0,0 +1,12 @@
+/*
+    determine class of boxer
+    date: 4/10/2023
+*/
+
+# include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
