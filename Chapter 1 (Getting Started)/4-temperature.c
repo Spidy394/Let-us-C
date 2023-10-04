60f7d09f313fd23f7260954c05abfd6e2dd2eb99
Shubhodeep Mondal <79023855+Spidy394@users.noreply.github.com>
2023-10-04 23:01:55 +0530

    added comments to 4-temperature.c

--- Chapter 1 (Getting Started)/4-temperature.c
+++ Chapter 1 (Getting Started)/4-temperature.c
@@ -7,13 +7,17 @@
 
 int main()
 {
+    // variable declation
     float f, c;
-    
+
+    // taking input of temperature in fahrenhit
     printf("Enter temperature in  Fahrenheit: ");
     scanf("%f", &f);
 
+    // converting fahrenheit
     c = ((f - 32)*5)/9;
 
+    // printint result
     printf("Temperature in Centigrade is: %.2f" ,c);
     
     return 0;
