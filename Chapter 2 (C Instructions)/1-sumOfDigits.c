540a102ad33dd043c1a64f04ff43ebf92ba0ebed
Shubhodeep Mondal <79023855+Spidy394@users.noreply.github.com>
2023-10-07 22:14:29 +0530

    added comments 1-sumOfDigits.c

--- Chapter 2 (C Instructions)/1-sumOfDigits.c
+++ Chapter 2 (C Instructions)/1-sumOfDigits.c
@@ -7,30 +7,29 @@
 
 int main()
 {
+    // declaration variables
     int n,sum;
 
+    // taking inputs 
     printf("Enter a 5-digit no: ");
     scanf("%d", &n);
 
-      //calculating the one's place number
-    
+    //calculating the one's place number
     sum = n%10;
-    //calculating the two's place number
     
+    //calculating the two's place number
     sum = sum + (n%100)/10;
     
     //calculating the third's place number
-    
     sum = sum + (n%1000)/100;
     
     //calculating the forth's place number
-    
     sum = sum + (n%10000)/1000;
     
     //calculating fifth's place number
-    
     sum = sum + (n%100000)/10000;
-   
+
+    // print result
     printf("The sum of digits of %d is %d" , n , sum);
     
     return 0;
