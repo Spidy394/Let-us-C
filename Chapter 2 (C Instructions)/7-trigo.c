88c5e3b4ed91099858782273ea87d70cc3fa70ee
Shubhodeep Mondal <79023855+Spidy394@users.noreply.github.com>
2023-10-07 22:32:37 +0530

    added comments to 7-trigo.c

--- Chapter 2 (C Instructions)/7-trigo.c
+++ Chapter 2 (C Instructions)/7-trigo.c
@@ -4,20 +4,25 @@
 */
 
 # include <stdio.h>
-# include <math.h>
+# include <math.h> // including maths library to use trigo. functions
 int main()
 {
+    // variable declaration 
     float inDegree, inRadian, sinValue, cosValue, tanValue; 
 
+    // take inputs 
     printf("Enter angle in Degree: ");
     scanf("%f", &inDegree);
 
+    // degree to radian comvertion
     inRadian = inDegree * 3.14 / 180 ; 
 
+    // trigo. operaions 
     sinValue = sin(inRadian);
     cosValue = cos(inRadian);
     tanValue = tan(inRadian);
 
+    // print resuts 
     printf("sin %.2f = %.2f\n", inDegree, sinValue);
     printf("cos %.2f = %.2f\n", inDegree, cosValue);
     printf("tan %.2f = %.2f\n", inDegree, tanValue);
