fd333cb484a5c67d2aacf3df113130da3acac5ed
Shubhodeep Mondal <79023855+Spidy394@users.noreply.github.com>
2023-10-07 22:19:02 +0530

    added comments 3-areaOfTriangle.c

--- Chapter 2 (C Instructions)/3-areaOfTriangle.c
+++ Chapter 2 (C Instructions)/3-areaOfTriangle.c
@@ -8,9 +8,11 @@
 
 int main()
 {
+    // variable declaration 
     int a,b,c;
     float s, area;
 
+    // take inputs
     printf("Enter side a: ");
     scanf("%d", &a);
     printf("Enter side b: ");
@@ -18,9 +20,11 @@ int main()
     printf("Enter side c: ");
     scanf("%d", &c);
 
+    // calaculation 
     s = (a+b+c)/2;
     area = sqrt(s*(s-a)*(s-b)*(s-c));
-    
+
+    // print result    
     printf("Area of triangle is: %.2f", area);
 
     return 0;
